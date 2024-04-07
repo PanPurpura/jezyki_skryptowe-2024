ca97618c8c46fc43812ead165e2b3f44d036cc99
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-04-07 18:59:13 +0200

    4.0 requirement

--- Zadanie_2/actions/actions.py
+++ Zadanie_2/actions/actions.py
@@ -70,4 +70,71 @@ class ActionCheckIfOpen(Action):
 
         return[]
     
+class ActionPlaceOrder(Action):
+    def name(self) -> Text:
+        return "action_place_order"
+    
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        file = open('actions/menu.json')
+        menu = json.load(file)
+        food = tracker.get_slot('item')
+        flag = False
+
+        for i in menu['items']:
+            if i['name'] == food:
+                flag = True
+                break
+        
+        if flag == False:
+            dispatcher.utter_message(text=f"We don't have this in our menu")
+            return [SlotSet('item', None)]
+        else:
+            dispatcher.utter_message(text=f"Do you have special conditions for the order?")
+            return []
+        
+class ActionHandleConditions(Action):
+    def name(self) -> Text:
+        return "action_handle_conditions"
+    
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        condition = tracker.get_slot('new_item')
+        food = tracker.get_slot('item')
+        quantity = tracker.get_slot('quantity')
+
+        order = food + ", " + quantity + " " + condition
+        orders = tracker.get_slot('orders') or []
+
+        orders.append(order)
+
+        dispatcher.utter_message(f"Adding your order to the list")
+        dispatcher.utter_message(f"Do you want to place another order?")
+        return [SlotSet('item', None), SlotSet('new_item', None), SlotSet('quantity', None), SlotSet('orders', orders)]
+        
+class ActionShowMenu(Action):
+    def name(self) -> Text:
+        return "action_show_orders"
+    
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        orders_ = tracker.get_slot('orders') or []
+
+        if len(orders_) == 0:
+            dispatcher.utter_message(text=f"Your order list is empty!")
+        else:
+            returned = ""
+            for food in orders_:
+                returned += food + ", \n"
+            dispatcher.utter_message("Here is your list of orders:")
+            dispatcher.utter_message(text=returned)
+                
+
+        return []
+    
 

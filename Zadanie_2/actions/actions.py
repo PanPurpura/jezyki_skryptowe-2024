3164b4af429ccab07bb924bdddc509c1af33663a
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-04-07 18:56:46 +0200

    3.5 requirement

--- Zadanie_2/actions/actions.py
+++ Zadanie_2/actions/actions.py
@@ -25,5 +25,49 @@ class ActionGetOpeningHours(Action):
 
 
         return []
+
+class ActionGetMenu(Action):
+    def name(self) -> Text:
+        return "action_get_menu"
+    
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        file = open('actions/menu.json')
+        menu = json.load(file)
+        returned = ""
+
+        for first in menu['items']:
+            returned += (f"- {first['name']}, price: {first['price']}\n")
+
+        dispatcher.utter_message(text="Here is the menu:") 
+        dispatcher.utter_message(text=returned)
+
+        return []
+    
+class ActionCheckIfOpen(Action):
+    def name(self) -> Text:
+        return "action_check_if_open"
+    
+    async def run(
+            self, dispatcher, tracker: Tracker, domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+        
+        file = open('actions/hours.json')
+        loaded = json.load(file)
+        hours = loaded['items']
+
+        dt = datetime.datetime.now()
+
+        time = dt.hour
+        if time >= hours[dt.strftime('%A')]['open'] and time <= hours[dt.strftime('%A')]['close']:
+            dispatcher.utter_message(text=f"Today is: {dt.strftime('%A')}, {dt.strftime('%H:%M:%S')}")
+            dispatcher.utter_message(text=f"The restaurant is open now")
+        else:
+            dispatcher.utter_message(text=f"Today is: {dt.strftime('%A')}, {dt.strftime('%H:%M:%S')}")
+            dispatcher.utter_message(text=f"The restaurant is closed now")
+
+        return[]
     
 
